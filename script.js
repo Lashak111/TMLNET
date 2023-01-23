097bc000cb61718b01d105b3f93e6547b71bcf79
Lashak111 <lkaraulashvili01@gmail.com>
2023-01-24 01:05:38 +0400

    24 january finnal commit

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
-const toggleNav = () => {
-  if (!menuOpen) {
-    Burgermenu.classList.add("open");
-    menuOpen = true;
+// const toggleNav = () => {
+//   if (!menuOpen) {
+//     Burgermenu.classList.add("open");
+//     menuOpen = true;
 
-    nav.classList.add("show");
-  } else {
-    nav.classList.remove("show");
-    Burgermenu.classList.remove("open");
-    menuOpen = false;
-  }
-};
+//     nav.classList.add("show");
+//   } else {
+//     nav.classList.remove("show");
+//     Burgermenu.classList.remove("open");
+//     menuOpen = false;
+//   }
+// };
 
-let Burgermenu = document.querySelector(".menu-btn");
+// let Burgermenu = document.querySelector(".menu-btn");
 
-let menuOpen = false;
-const nav = document.getElementById("nav-items");
+// let menuOpen = false;
+// const nav = document.getElementById("nav-items");
 
-Burgermenu.addEventListener("click", () => {
-  toggleNav();
-});
+// Burgermenu.addEventListener("click", () => {
+//   toggleNav();
+// });
 
-nav.addEventListener("click", () => {
-  toggleNav();
+// nav.addEventListener("click", () => {
+//   toggleNav();
+// });
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+  //Links
+  navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+
+  //Animation
+  hamburger.classList.toggle("toggle");
 });
