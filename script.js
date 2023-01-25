92fc2bf6f044a7e30129562c082b285e743f4bb1
Lashak111 <lkaraulashvili01@gmail.com>
2023-01-25 12:28:09 +0400

    4 commit

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-// const toggleNav = () => {
-//   if (!menuOpen) {
-//     Burgermenu.classList.add("open");
-//     menuOpen = true;
-
-//     nav.classList.add("show");
-//   } else {
-//     nav.classList.remove("show");
-//     Burgermenu.classList.remove("open");
-//     menuOpen = false;
-//   }
-// };
-
-// let Burgermenu = document.querySelector(".menu-btn");
-
-// let menuOpen = false;
-// const nav = document.getElementById("nav-items");
-
-// Burgermenu.addEventListener("click", () => {
-//   toggleNav();
-// });
-
-// nav.addEventListener("click", () => {
-//   toggleNav();
-// });
 
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
