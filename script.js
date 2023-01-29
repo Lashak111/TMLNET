9d61a843b2ee714ff24a35dbef23a968e486d5a6
Lashak111 <lkaraulashvili01@gmail.com>
2023-01-29 16:58:28 +0400

    header responsive new commit

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// const toggleNav = () => {
+//   if (!menuOpen) {
+//     Burgermenu.classList.add("open");
+//     menuOpen = true;
+
+//     nav.classList.add("show");
+//   } else {
+//     nav.classList.remove("show");
+//     Burgermenu.classList.remove("open");
+//     menuOpen = false;
+//   }
+// };
+
+// let Burgermenu = document.querySelector(".menu-btn");
+
+// let menuOpen = false;
+// const nav = document.getElementById("nav-items");
+
+// Burgermenu.addEventListener("click", () => {
+//   toggleNav();
+// });
+
+// nav.addEventListener("click", () => {
+//   toggleNav();
+// });
 
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
